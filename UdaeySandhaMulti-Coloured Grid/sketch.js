6293c49b58297f7a2c19145537f5c1b048e0c048
5802269 <145162236+5802269@users.noreply.github.com>
2023-10-06 11:34:57 -0600

    new

--- UdaeySandhaMulti-Coloured Grid/sketch.js
+++ UdaeySandhaMulti-Coloured Grid/sketch.js
@@ -4,43 +4,53 @@
 
 // Global Variables
 let gridSize, colors;
-
+//----------------------------------------------------------------------------------------------------------------
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  document.addEventListener("contextmenu", event => event.preventDefault());
-  gridSize = floor(random(5, 15));
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
+  gridSize = floor(random(5, 15)); //random grideSize at start
   generateColors();
 }
-
-
-
-function mousePressed(){
+//----------------------------------------------------------------------------------------------------------------
+function mousePressed() {
   if (mouseButton === LEFT) {
+    //left makes gridesize small
     gridSize++;
     generateColors();
   } else if (mouseButton === RIGHT) {
-    gridSize = max(1, gridSize - 1); // Ensure gridSize is always at least 1
+    //right makes gridesize bigger
+    print(gridSize);
+    gridSize = max(1, gridSize - 1); //max gridsize
     generateColors();
   }
 }
-
+//----------------------------------------------------------------------------------------------------------------
 function keyPressed() {
+  //changing the colors when key pressed
   generateColors();
 }
-
+//----------------------------------------------------------------------------------------------------------------
 function generateColors() {
-  let iColor,jColor;
+  //generating semi random colors and puting them in a array
+  let iColor, jColor;
   colors = [];
   for (let i = 0; i < gridSize * gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
-      iColor=map(i,0,gridSize,0,255),
-      jColor=map(j,0,gridSize,0,255),
-      colors.push(color(Math.abs(iColor*-random(1,1.5)),Math.abs(jColor*-random(1,1.5)),jColor));
+      (iColor = map(i, 0, gridSize, 0, 255)),
+        (jColor = map(j, 0, gridSize, 0, 255)),
+        colors.push(
+          color(
+            Math.abs(iColor - 255),
+            Math.abs(jColor - random(0, 255)),
+            jColor
+          )
+        );
     }
   }
 }
-
-function drawGrid(){
+//----------------------------------------------------------------------------------------------------------------
+function drawGrid() {
+  //drawing the grid
   let index = 0;
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
@@ -48,12 +58,15 @@ function drawGrid(){
       let gridHeight = height / gridSize;
       let x = j * gridWidth;
       let y = i * gridHeight;
-      fill(colors[index]);
-      rect(x, y, gridWidth, gridHeight);
+      fill(colors[index]); // accessing the array to get the colors
+      rect(x, y, gridWidth, gridHeight); //drawing the grid
       index++;
-}}}
-
+    }
+  }
+}
+//----------------------------------------------------------------------------------------------------------------
 function draw() {
   drawGrid();
-  
 }
+//----------------------------------------------------------------------------------------------------------------
+//End
