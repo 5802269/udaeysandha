130f01b775fd4961f871541e22451c7baf09a72f
5802269 <145162236+5802269@users.noreply.github.com>
2023-10-06 01:04:53 -0600

    new

--- UdaeySandhaMulti-Coloured Grid/sketch.js
+++ UdaeySandhaMulti-Coloured Grid/sketch.js
@@ -1,57 +1,59 @@
-let gridSize;
-let grid;
-let colors = [];
-let bgColor;
+// Multi-Coloured Grid
+// Udaey Sandha
+// Sept 28, 2023
+
+// Global Variables
+let gridSize, colors;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noStroke();
+  document.addEventListener("contextmenu", event => event.preventDefault());
+  gridSize = floor(random(5, 15));
   generateColors();
-  calculateGridSize();
-  createGrid();
 }
 
-function draw() {
-  background(bgColor);
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      fill(colors[i][j]);
-      rect(j * (width / gridSize), i * (height / gridSize), width / gridSize, height / gridSize);
-    }
+
+
+function mousePressed(){
+  if (mouseButton === LEFT) {
+    gridSize++;
+    generateColors();
+  } else if (mouseButton === RIGHT) {
+    gridSize = max(1, gridSize - 1); // Ensure gridSize is always at least 1
+    generateColors();
   }
-}
-
-function calculateGridSize() {
-  gridSize = floor(random(5, 15)); // Adjust this range for more or fewer squares
-}
-
-function createGrid() {
-  grid = new Array(gridSize);
-  colors = new Array(gridSize);
-  for (let i = 0; i < gridSize; i++) {
-    grid[i] = new Array(gridSize);
-    colors[i] = new Array(gridSize);
-    for (let j = 0; j < gridSize; j++) {
-      grid[i][j] = random(2); // 0 or 1 for square color
-      colors[i][j] = color(random(255), random(255), random(255));
-    }
-  }
-}
-
-function generateColors() {
-  bgColor = color(random(255), random(255), random(255));
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      colors[i][j] = color(random(255), random(255), random(255));
-    }
-  }
-}
-
-function mouseClicked() {
-  calculateGridSize();
-  createGrid();
 }
 
 function keyPressed() {
   generateColors();
+}
+
+function generateColors() {
+  let iColor,jColor;
+  colors = [];
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      iColor=map(i,0,gridSize,0,255),
+      jColor=map(j,0,gridSize,0,255),
+      colors.push(color(Math.abs(iColor*-random(1,1.5)),Math.abs(jColor*-random(1,1.5)),jColor));
+    }
+  }
+}
+
+function drawGrid(){
+  let index = 0;
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      let gridWidth = width / gridSize;
+      let gridHeight = height / gridSize;
+      let x = j * gridWidth;
+      let y = i * gridHeight;
+      fill(colors[index]);
+      rect(x, y, gridWidth, gridHeight);
+      index++;
+}}}
+
+function draw() {
+  drawGrid();
+  
 }
