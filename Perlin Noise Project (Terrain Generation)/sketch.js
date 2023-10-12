eb83ebce99574d6de701f7e740f7c60616d3d40f
5802269 <145162236+5802269@users.noreply.github.com>
2023-10-11 21:52:41 -0600

    new

--- Perlin Noise Project (Terrain Generation)/sketch.js
+++ Perlin Noise Project (Terrain Generation)/sketch.js
@@ -2,58 +2,61 @@
 // Udaey Sandha
 // October 6, 2023
 
+let yFlag,
+  xFlag,
+  average,
+  timeMoving = 0,
+  yValue,
+  yValueTime = 0;
 
-let timeMoving = 0,rectWidth = 0.5, yValueIndex, yValue, yValueTime = 0, noiseShift = 0.01, yValues = [], minYValue, xValues = [];
+const RECT_WIDTH = 0.5,
+  NOISE_SHIFT = 0.01,
+  FLAG_HEIGHT = 50,
+  MOVE_BY = 0.05;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // drawTerrain();
-  // drawFlag();
 }
 
-function drawTerrain() {
-  yValues=[];
-  xValues=[];
-  for (let x = 0; x < width; x += rectWidth) {
+function generateTerrain() {
+  yFlag = height;
+  xFlag = 0;
+  average = 0;
+  let i = 0;
+  for (let x = 0; x < width; x += RECT_WIDTH) {
     noFill();
     stroke(0);
     strokeWeight(2);
     yValue = noise(yValueTime);
     yValue = map(yValue, 0, 1, 0, height);
-    yValueTime += noiseShift;
+    yValueTime += NOISE_SHIFT;
     rectMode(CORNERS);
-    rect(x, height, x + rectWidth, yValue);
-    yValues.push(yValue);
-    xValues.push(x + rectWidth);
-    
-
+    rect(x, height, x + RECT_WIDTH, yValue);
+    if (yFlag >= yValue) (yFlag = yValue), (xFlag = x + RECT_WIDTH);
+    average += yValue;
+    i++;
   }
+  average = average / i;
   yValueTime = timeMoving;
 }
 
-function drawFlag() {
-  let flagHeight=50;
-  minYValue = min(yValues);
-  yValueIndex = yValues.indexOf(minYValue);
-  let x = xValues[yValueIndex];
+function drawFlag(x, y) {
   stroke("brown");
-  line(x, minYValue, x, minYValue - flagHeight);
+  line(x, y, x, y - FLAG_HEIGHT);
   fill("red");
-  if (x+flagHeight<width) triangle( x, minYValue - flagHeight,x,minYValue-flagHeight/2,x+flagHeight/2,minYValue-flagHeight*0.75);
-  else triangle( x, minYValue - flagHeight,x,minYValue-flagHeight/2,x-flagHeight/2,minYValue-flagHeight*0.75);
+  if (x + FLAG_HEIGHT < width) triangle(x, y - FLAG_HEIGHT, x, y - FLAG_HEIGHT / 2, x + FLAG_HEIGHT / 2, y - FLAG_HEIGHT * 0.75);
+  else triangle(x, y - FLAG_HEIGHT, x, y - FLAG_HEIGHT / 2, x - FLAG_HEIGHT / 2, y - FLAG_HEIGHT * 0.75);
 }
 
-function averageHeight(){
-  let average=(max(yValue)+min(yValue))/2;
+function averageHeight() {
   strokeWeight(7);
-  line(0,average,width,average);
+  line(0, average, width, average);
 }
 
 function draw() {
   background(220);
-  drawTerrain();
-  drawFlag();
-  timeMoving+=0.05;
-  drawFlag();
+  generateTerrain();
+  timeMoving += MOVE_BY;
+  drawFlag(xFlag, yFlag);
   averageHeight();
 }
