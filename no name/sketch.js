80419c6c01bf8f2be7fc303bd9dac7ac2b69ed6a
5802269 <145162236+5802269@users.noreply.github.com>
2023-10-25 23:25:56 -0600

    new

---
+++ no name/sketch.js
@@ -0,0 +1,27 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+  drawShape();
+}
+
+function drawShape() {
+  noFill();
+  for (let y=50; y<height-50;y+=9){
+    for (let x=0; x<width;x++){
+      strokeWeight(1);
+      bezier(0, y, 20, y-150, 40, y+150, 80, y);
+      bezier(80, y, 120, y-150, 160, y+150, 240, y);
+      bezier(240, y, 320, y-150, 400, y+150, 500, y);
+      bezier(500, y, 550, y-50, 650, y-50, 900, y-30);
+    }
+  }
+ 
+}
