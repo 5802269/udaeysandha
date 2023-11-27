c22bffae6e5f775084828ecb3566a135dcafac98
5802269 <145162236+5802269@users.noreply.github.com>
2023-11-27 00:14:28 -0600

    grbku

--- udaeySandhaCircleInACircle/sketch.js
+++ udaeySandhaCircleInACircle/sketch.js
@@ -10,18 +10,23 @@ function setup() {
   if (width<height) circleSize=width/2;
   else circleSize=height/2;
   noFill();
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(220);
-  circleInACircle(width/2,height/2,5,circleSize);
+  circleInACircle(width/2,height/2,3,circleSize);
 }
 
 function circleInACircle(x,y,n,s){
-  if (circleSize>10){
+  if (s>10){
     circle(x,y,s);
-    circle(x-2*x/6,y,s/3);
-    circle(x+2*x/6,y,s/3);
+    let R = s/2;
+    let a = 360/(2*n);
+    let r = sin(a)*R/(1+sin(a));
+    circle(x+R-r,y,2*r);
+    print(x+(R-r)*cos(2*a));
+    circle(x+(R-r)*cos(2*a),y+(R-r)*sin(2*a),2*r);
     
 }
 
