b9fe0f15063ed3a637372ec4c4be4da6efa6f60b
5802269 <145162236+5802269@users.noreply.github.com>
2023-12-06 15:24:19 -0600

    n

--- 111final/sketch.js
+++ 111final/sketch.js
@@ -5,11 +5,46 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let b = p5.board('/dev/cu.usbmodem1421', 'arduino');
+let led;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  led = b.pin(9, 'LED');
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW){
+    led.on();
+  } else if (keyCode === RIGHT_ARROW) {
+    led.off();
+  } else if (keyCode === UP_ARROW){
+    led.blink();
+    console.log('Hello, World!');
+  } else if (keyCode === DOWN_ARROW) {
+    led.noBlink();
+  }
 }
 
 function draw() {
   background(220);
 }
+
+// let sensorVal = 0;
+
+// function preload() {
+//   loadBoard();
+// }
+
+// function setup() { 
+//   const sensor = new five.Sensor('A0');
+//   sensor.on('change', () => {
+//     sensorVal = sensor.scaleTo(0, 300);
+//   });
+//   createCanvas(400, 400);
+// } 
+
+// function draw() { 
+//   background('blue');
+//   rect(50, 150, sensorVal, 100);
+// }
