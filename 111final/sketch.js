de389dbc6d5ffe56b7e1a52f009cb98552503e64
5802269 <145162236+5802269@users.noreply.github.com>
2023-12-11 17:07:12 -0600

    got the library to work in the vs code and test the servo

--- 111final/sketch.js
+++ 111final/sketch.js
@@ -1,76 +1,12 @@
-let sensorVal = 0;
-
 function preload() {
   loadBoard();
 }
 
-function setup() { 
-  const sensor = new five.Sensor('A0');
-  sensor.on('change', () => {
-    sensorVal = sensor.scaleTo(0, 300);
-  });
-  createCanvas(400, 400);
-} 
-
-function draw() { 
-  background('blue');
-  rect(50, 150, sensorVal, 100);
+function setup() {
+  const motor = new five.Motor([2, 3]);
+  motor.reverse(255);
 }
 
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
-// let b = p5.board("/dev/cu.usbmodem1421", "arduino");
-// let led;
-
-// function setup() {
-//   led = b.pin(9, "LED");
-// }
-
-// function keyPressed() {
-//   if (keyCode === LEFT_ARROW){
-//     led.on();
-//   } else if (keyCode === RIGHT_ARROW) {
-//     led.off();
-//   } else if (keyCode === UP_ARROW){
-//     led.blink();
-//     console.log("Hello, World!"); 
-//   } else if (keyCode === DOWN_ARROW) {
-//     led.noBlink();
-//   }
-// }
-
-// let sensorVal = 0;
-
-// function preload() {
-//   loadBoard();
-// }
-
-// function setup() { 
-//   const sensor = new five.Sensor('A0');
-//   sensor.on('change', () => {
-//     sensorVal = sensor.scaleTo(0, 300);
-//   });
-//   createCanvas(400, 400);
-// } 
-
-// function draw() { 
-//   background('blue');
-//   rect(50, 150, sensorVal, 100);
-// }
-// const { Board, Led } = require("johnny-five");
-// const board = new Board();
-
-// board.on("ready", () => {
-
-//   // Create a standard `led` component instance
-//   const led = new Led(13);
-
-//   // "blink" the led in 500ms
-//   // on-off phase periods
-//   led.blink(500);
-// });
+function draw() {
+  // draw stuff
+}
