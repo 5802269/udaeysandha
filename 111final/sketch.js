668eb513dfbc81bc111edfa7ea42ebe46f7b7dac
5802269 <145162236+5802269@users.noreply.github.com>
2023-12-14 15:22:32 -0600

    serialport.js tryed it out

--- 111final/sketch.js
+++ 111final/sketch.js
@@ -1,30 +1,17 @@
-// serial port initialization:
-var SerialPort = require("serialport"); // include the serialport library
-var portName = process.argv[2]; // get the port name from the command line
-var myPort = new SerialPort(portName, 9600);// open the port
- 
-// these are the definitions for the serial events:
-myPort.on('open', openPort); // called when the serial port opens
- 
-function openPort() {
-  var brightness = 0; // the brightness to send for the LED
-  console.log('port open');
-  console.log('baud rate: ' + myPort.options.baudRate);
- 
-  // since you only send data when the port is open, this function
-  // is local to the openPort() function:
-  function sendData() {
-    // convert the value to an ASCII string before sending it:
-    myPort.write(brightness.toString());
-    console.log('Sending ' + brightness + ' out the serial port');
-    // increment brightness by 10 points. Rollover if < 255:
-    if (brightness < 255) {
-      brightness+= 10;
-    } else {
-      brightness = 0;
-  }
+let serial;
+
+function setup(){
+  createCanvas(320,240);
+  serial = new p5.SerialPort();
+  serial.open("USB\VID_2341&PID_0043\55736313937351C04001");
 }
- 
-// set an interval to update the brightness 2 times per second:
-setInterval(sendData, 500);
+
+function draw(){
+  background("#3399EE");
+  fill(255);
+  text(mouseY,width/2,height/2);
+}
+
+function mouseDragged(){
+  serial.write(mouseY);
 }
