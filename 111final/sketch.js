3ef7af74d9b47a204517d904c7f0e5f71c1c48fd
5802269 <145162236+5802269@users.noreply.github.com>
2024-01-18 23:39:20 -0600

    finished workng on the blockly. now have a user interface . need to link it to wel serial liibrary

--- 111final/sketch.js
+++ 111final/sketch.js
@@ -1,24 +1,24 @@
-// let port, reader, writer;
+let port, reader, writer;
 
-// async function setup() {
-// 	createCanvas(windowWidth, windowHeight);
-// 	background("black");
+async function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background("black");
 
-// 	noLoop();
-// 	({ port, reader, writer } = await getPort());
-// 	loop();
-// }
+	noLoop();
+	({ port, reader, writer } = await getPort());
+	loop();
+}
 
-// async function draw() {	if (port) {
-// 	try {
-// 		if (mouseIsPressed) {
-// 			background("RED");
-// 			await writer.write("clicked!\n");
-// 		}
-// 		else {
-// 			background("BLACK");
-// 			await writer.write("not clicked!\n");
-// 		}
-// 	} catch (e) { console.error(e) }
-// }
-// }
+async function draw() {	if (port) {
+	try {
+		if (mouseIsPressed) {
+			background("RED");
+			await writer.write("clicked!\n");
+		}
+		else {
+			background("BLACK");
+			await writer.write("not clicked!\n");
+		}
+	} catch (e) { console.error(e) }
+}
+}
